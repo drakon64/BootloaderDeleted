c88bd84b5bc5561a0799f39efe18cd5af7ffd9d5
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-15 22:12:28 +0000

    Remove OpenAPI

--- src/Bot/Program.cs
+++ src/Bot/Program.cs
@@ -18,15 +18,7 @@ public class Program
             );
         });
 
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-
         var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
 
         Todo[] sampleTodos =
         [
